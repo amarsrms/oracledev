7eafaad2961390504a70d148a989b8febaf533f8
amarsrms <amarpal.srmscs@gmail.com>
2019-02-06 16:14:07 +0530

    Added first Java program

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld
+{
+public static void main(String[] args)
+{
+System.out.println("\t------------------------------TABLE--------------------------------");
+for (int i=1;i<=10;i++)
+{
+for ( int j=2;j<=10;j++)
+System.out.print("\t| "+i*j);
+System.out.println("\n");
+}
+System.out.println("\t------------------------------END--------------------------------");
+}
+} 
